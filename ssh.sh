d199f0658a001d00bcf157392d731fe476da1823
yuemengyi <1251699512@qq.com>
2019-06-19 19:28:06 -0700

    ssh.sh

---
+++ ssh.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#install ssh
+apt-get install -y ssh
+
+#configure sshd
+CONFIG=/etc/ssh/sshd_config
+sed -i s?"prohibit-password"?"yes"?g ${CONFIG}
+
+#restart ssh service
+systemctl reestart sshd
